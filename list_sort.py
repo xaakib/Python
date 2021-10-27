d8b2d4df50e850a0379bfa313dc1fdf168c05f01
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-27 12:30:44 +0600

    intersection with union

---
+++ list_sort.py
@@ -0,0 +1,18 @@
+li = ['shakib','deep','arif']
+print(li)
+li.append('xaakibx')
+print(li)
+li.sort
+print(li)
+fruits = ['apple','bannena','cherry']
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+x = 'awesome'
+def myfun():
+  x = 'fantastic'
+  print('Python is '+x)
+myfun()
+print("Python is" +x)
---
+++ set_interrsction.py
@@ -0,0 +1,7 @@
+x = {'apple','banana','cherry'}
+y = {'apple','ftuits','xaakib'}
+f = x.intersection(y)
+t =  x.union(y)
+print(t)
+print(f)
+
