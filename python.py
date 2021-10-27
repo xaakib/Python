a37f2f2b94ae71fd730bbeafc093e27e179ab6dd
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-27 10:10:20 +0600

    G

---
+++ python.py
@@ -0,0 +1,22 @@
+li = ['shakib', 'deep', 'arif']
+print(li)
+li.append('xaakibx')
+print(li)
+li.sort
+print(li)
+fruits = ['apple', 'bannena', 'cherry']
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = input()
+
+
+def myfun():
+    x = 'fantastic'
+    print('Python is ' + x)
+
+
+myfun()
+print("Python is" + x)
